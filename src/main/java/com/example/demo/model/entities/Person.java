43699c08d76145e3f3213da0a3dec721916ae343
Camilo Andres Ferrer bustos <caferrerb@MacBook-Pro-de-Camilo-2.local>
2020-10-29 08:41:06 -0500

    ejemplo

--- src/main/java/com/example/demo/model/entities/Person.java
+++ src/main/java/com/example/demo/model/entities/Person.java
@@ -1,49 +1,52 @@
 package com.example.demo.model.entities;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+
 
 @Entity
 @Table(name = "persons")
 public class Person implements Serializable {
 
   @Id
-  private Long id;
+  private Integer id;
 
+  @NotBlank
+  @NotEmpty
+  @NotNull
   private String name;
 
   @Column(name = "last_name")
-  @NotNull
   @NotEmpty
-  @Size(min = 2, max = 50)
+  @NotNull
   private String lastName;
 
   @Column(name = "phone_number")
+  @Size(min=5, max=10, message = "entre 5 y 10 digitos")
   private String phoneNumber;
 
   public Person() {
   }
 
-  public Person(String name, String lastName, String phoneNumber) {
+  public Person(Integer id, String name, String lastName, String phoneNumber) {
+    this.id = id;
     this.name = name;
     this.lastName = lastName;
     this.phoneNumber = phoneNumber;
   }
 
-  public Long getId() {
+  public Integer getId() {
     return id;
   }
 
-  public void setId(Long id) {
+  public void setId(Integer id) {
     this.id = id;
   }
 
